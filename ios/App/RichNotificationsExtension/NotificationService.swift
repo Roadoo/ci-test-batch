f8f96271358e2073c07e3562960a90356bcde094
clemdesign <contact@clemdesign.fr>
2023-01-19 14:51:09 +0100

    Add and configure Batch plugin

---
+++ ios/App/RichNotificationsExtension/NotificationService.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationService.swift
+//  RichNotificationsExtension
+//
+//  Created by Clément G on 19/01/2023.
+//
+
+import BatchExtension;
+
+class NotificationService: BAENotificationServiceExtension {
+    
+}
